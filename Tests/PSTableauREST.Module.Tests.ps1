a89ae7646b0974d491a6e1058f619ed374c95a75
Andrey Sluzhivoy <andrey.sluzhivoy@d-one.ai>
2023-11-13 10:47:27 +0100

    * module tests improved

--- Tests/PSTableauREST.Module.Tests.ps1
+++ Tests/PSTableauREST.Module.Tests.ps1
@@ -1,9 +1,15 @@
+BeforeAll {
+    Import-Module PSScriptAnalyzer
+    Import-Module Assert
+    # $script:VerbosePreference = 'Continue' # display verbose output of the tests
+    $script:DebugPreference = 'Continue' # display debug output of the tests
+}
 BeforeDiscovery {
     $script:ParentDir = (Get-Item $PSCommandPath).Directory.Parent.FullName
     $script:ModuleName = (Split-Path -Leaf $PSCommandPath) -Replace ".Module.Tests.ps1"
     $script:ModuleFile =     "$ParentDir/$ModuleName/$ModuleName.psm1"
     $script:ModuleManifest = "$ParentDir/$ModuleName/$ModuleName.psd1"
-    $script:CodeFiles = Get-ChildItem -Path "$ParentDir" -Filter *.ps1 -Recurse
+    $script:CodeFiles = Get-ChildItem -Path "$ParentDir" -Filter *.ps1 -Recurse | Resolve-Path -Relative
     $script:ScriptAnalyzerRules = Get-ScriptAnalyzerRule
     $script:ScriptAnalyzerResults = Invoke-ScriptAnalyzer -Path $ModuleFile -ExcludeRule PSUseBOMForUnicodeEncodedFile -Severity Error,Warning
 }
@@ -16,21 +22,28 @@ Describe "Module Structure and Validation Tests" -Tag Module -WarningAction Sile
 
         It "has the a manifest file of <ModuleName>" {
             "$ModuleManifest" | Should -Exist
+            "$ModuleManifest" | Should -FileContentMatch "$ModuleName.psm1"
         }
 
         It "<ModuleFile> contains valid PowerShell code" {
             $psFile = Get-Content -Path $ModuleFile -ErrorAction Stop
             $errors = $null
             $null = [System.Management.Automation.PSParser]::Tokenize($psFile, [ref]$errors)
+            if ($errors) {
+                Write-Debug ($errors | ConvertTo-Json -Compress)
+            }
             $errors.Count | Should -Be 0
         }
     }
 
     Context "Code Validation <file>" -ForEach $CodeFiles {
         It "<_> is valid PowerShell code" {
-            $psFile = $_
+            $fileItem = Get-Item -LiteralPath $_
             $errors = $null
-            [void][System.Management.Automation.Language.Parser]::ParseFile($psFile, [ref]$null, [ref]$errors)
+            $null = [System.Management.Automation.Language.Parser]::ParseFile($fileItem.FullName, [ref]$null, [ref]$errors)
+            if ($errors) {
+                Write-Debug ($errors | ConvertTo-Json -Compress)
+            }
             $errors.Count | Should -Be 0
         }
     }
